a0be82f82004984e48c313b5aa72c0abe101c567
Stepan Riha <github@nonplus.net>
2022-01-19 23:26:01 +0000

    Don't use contraction.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def respond(user_input):
     if "What is your name?" == user_input:
         return "My name is chatbot! What's yours?"
     else:
-        return "Sorry I didn't understand that."
+        return "Sorry I did not understand that."
 
 def main():
     while True:
